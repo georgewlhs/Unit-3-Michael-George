9160cac95a01728a3f8dfdf8bc68d92a0181ce90
Micahel George <georgem@wlwv.k12.or.us>
2019-10-03 09:50:28 -0700

    modified drinkingage added gitignore

--- DrinkingAge.java
+++ DrinkingAge.java
@@ -1,3 +1,13 @@
-public class DrinkingAge{
+import java.util.Scanner;
 
+public class DrinkingAge{
+  public static void main(String[] args){
+    Scanner scan = new Scanner(System.in);
+    System.out.println("Enter your current age: ");
+    int age = scan.nextInt();
+    if (age < 21)
+      System.out.println("You are too young to drink");
+    else
+      System.out.println("You are older enough to drink");
+  }
 }
